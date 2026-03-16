0ec75b7066e720cfde0a6307377dd794392bc104
Derrick Won <derrick.won@alphapod.com>
2026-03-16 18:18:12 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksGreenImage from '/Users/alphapod/Desktop/Learning_Projects/VueJS/Intro-to-Vue-3-Composition-API/src/assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const sizes = ref(['S', 'M', 'L', 'XL'])
+const soldout = ref(['M','S','L'])
 </script>
   
 <template>
@@ -19,6 +21,11 @@ const details = ref(['50% cotton', '30% wool', '20% polyester'])
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
+        <ul>
+          <li v-for="size in sizes" :key="size">
+            <p v-if="size">{{ size }} </p>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
